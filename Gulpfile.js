b47990d746d51964143cf0828603ec01e89462ae
Jorrit Tinholt <jorrit@decorrespondent.nl>
2016-08-15 17:19:21 +0200

    Added Gulp to auto-concat CSS files

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var watch = require('gulp-watch');
+
+var css_src = 'Mimoto/userinterface/templates/Mimoto.CMS/**/*.css';
+
+gulp.task('default', function () {
+    gulp.watch(css_src, ['concatCss']);
+});
+
+gulp.task('concatCss', function () {
+    return gulp.src(css_src)
+        .pipe(concatCss('_main.css'))
+        .on('error', function () {
+            this.emit('end');
+        })
+        .pipe(gulp.dest('web/static/css/'));
+});
