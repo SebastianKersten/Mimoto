8d5c288d9917f4841afd746e5aba6055c03dcf68
SebastianKersten <sebastiankersten@gmail.com>
2016-10-24 16:40:23 +0200

    Realtime typing via diffs

--- web/static/js/Mimoto.Aimless.js
+++ web/static/js/Mimoto.Aimless.js
@@ -632,6 +632,74 @@ Mimoto.Aimless.connect = function()
         // o.a. remote dashboard control
     });
 
+
+
+    Mimoto.Aimless.realtime.broadcastedValues = [];
+    Mimoto.Aimless.realtime.changes = [];
+    Mimoto.Aimless.realtime.registerChange = function(sFormName, sInputFieldId, value)
+    {
+        if (!Mimoto.Aimless.realtime.changes[sInputFieldId])
+        {
+            Mimoto.Aimless.realtime.changes[sInputFieldId] = {
+                sFormName: sFormName,
+                newValue: value
+            }
+        }
+        else
+        {
+            Mimoto.Aimless.realtime.changes[sInputFieldId].newValue = value;
+        }
+    }
+
+
+
+    Mimoto.Aimless.realtime.dmp = new diff_match_patch();
+
+
+    setInterval(function()
+    {
+        for (var sInputFieldId in Mimoto.Aimless.realtime.changes)
+        {
+            var field = Mimoto.Aimless.realtime.changes[sInputFieldId];
+
+
+            // #todo - presence - lastvalue of field store in user object
+
+
+            // calculate new value
+            var originalText = Mimoto.Aimless.realtime.broadcastedValues[sInputFieldId].value;
+            var modifiedText = Mimoto.Aimless.realtime.changes[sInputFieldId].newValue;
+
+            //var ms_start = (new Date).getTime();
+            var diff = Mimoto.Aimless.realtime.dmp.diff_main(originalText, modifiedText, true);
+            //var ms_end = (new Date).getTime();
+
+            if (diff.length > 2) {
+                Mimoto.Aimless.realtime.dmp.diff_cleanupSemantic(diff);
+            }
+
+            var patch_list = Mimoto.Aimless.realtime.dmp.patch_make(originalText, modifiedText, diff);
+            patch_text = Mimoto.Aimless.realtime.dmp.patch_toText(patch_list);
+
+
+            // setup
+            var data = {
+                fieldId: sInputFieldId,
+                diff: patch_text
+            };
+
+            // broadcast update
+            Mimoto.Aimless.privateChannel.trigger('client-Aimless:formfield_update_' + field.sFormName, data);
+
+            // store
+            Mimoto.Aimless.realtime.broadcastedValues[sInputFieldId].value = field.newValue;
+            delete Mimoto.Aimless.realtime.changes[sInputFieldId];
+
+        }
+
+    }, 100); // send every 100 milliseconds if position has changed
+
+
 }
 
 
@@ -716,12 +784,22 @@ Mimoto.form.closeForm = function(sFormName)
     Mimoto.Aimless.privateChannel.bind('client-Aimless:formfield_update_' + sFormName, function(data)
     {
 
-        console.log(data.fieldId + ' = ' + data.value);
-
         var $input = $("input[mls_form_input='" + data.fieldId + "']");
 
-        $($input).val(data.value);
 
+        // 1. check if supports realtime
+        // 2. get this text
+        // 3. get diff patch
+
+
+        var currentValue = $($input).val();
+        var patches = Mimoto.Aimless.realtime.dmp.patch_fromText(data.diff);
+
+        //var ms_start = (new Date).getTime();
+        var results = Mimoto.Aimless.realtime.dmp.patch_apply(patches, currentValue);
+        //var ms_end = (new Date).getTime();
+
+        $($input).val(results[0]);
     });
 }
 
@@ -1014,58 +1092,3 @@ Mimoto.popup.close = function()
     layer_overlay.classList.add('hidden');
     layer_popup.classList.add('hidden');
 }
-
-
-
-Mimoto.Aimless.realtime.broadcastedValues = [];
-Mimoto.Aimless.realtime.changes = [];
-Mimoto.Aimless.realtime.registerChange = function(sFormName, sInputFieldId, value)
-{
-
-    var now = Date.parse(new Date());
-
-    if (!Mimoto.Aimless.realtime.changes[sInputFieldId])
-    {
-        Mimoto.Aimless.realtime.changes[sInputFieldId] = {
-            sFormName: sFormName,
-            newValue: value,
-            created: now,
-            modified: now
-        }
-    }
-    else
-    {
-        Mimoto.Aimless.realtime.changes[sInputFieldId].newValue = value;
-        Mimoto.Aimless.realtime.changes[sInputFieldId].modified = now;
-
-    }
-}
-
-setInterval(function()
-{
-    for (var sInputFieldId in Mimoto.Aimless.realtime.changes)
-    {
-        var field = Mimoto.Aimless.realtime.changes[sInputFieldId];
-
-        //if (field.modified - field.created  > 100)
-        {
-            // calculate new value
-            var changedValue = Mimoto.Aimless.realtime.changes[sInputFieldId].newValue;
-
-            // setup
-            var data = {
-                fieldId: sInputFieldId,
-                value: field.newValue
-            };
-
-            // broadcast update
-            Mimoto.Aimless.privateChannel.trigger('client-Aimless:formfield_update_' + field.sFormName, data);
-
-            // store
-            Mimoto.Aimless.realtime.broadcastedValues[sInputFieldId].value = field.newValue;
-            delete Mimoto.Aimless.realtime.changes[sInputFieldId];
-        }
-
-    }
-
-}, 100); // send every 100 milliseconds if position has changed
