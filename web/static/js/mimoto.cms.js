1a0f51d742f16e6aaf247474c22f8fac10bcca87
Sebastian Kersten <sebastiankersten@gmail.com>
2017-09-20 17:29:11 +0200

    Popup overlay onclick closes popup in cms

--- web/static/js/mimoto.cms.js
+++ web/static/js/mimoto.cms.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// __webpack_hash__
-/******/ 	__webpack_require__.h = "46626055f9ce7c2625cc";
+/******/ 	__webpack_require__.h = "6f2d46ffef5201f245ca";
 /******/
 /******/ 	// __webpack_chunkname__
 /******/ 	__webpack_require__.cn = "js/mimoto.cms.js";
