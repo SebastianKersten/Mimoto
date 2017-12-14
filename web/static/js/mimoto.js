e59150f38bc04ca43eb89bde651f571c8ac6f34f
Heleen Emanuel <heleen@momkai.com>
2017-12-14 17:00:42 +0100

    Fix for action title font styling and height

--- web/static/js/mimoto.js
+++ web/static/js/mimoto.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// __webpack_hash__
-/******/ 	__webpack_require__.h = "2a61d72a1e37e2d4567d";
+/******/ 	__webpack_require__.h = "419039eff4948c15f114";
 /******/
 /******/ 	// __webpack_chunkname__
 /******/ 	__webpack_require__.cn = "js/mimoto.js";
